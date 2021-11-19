96b74598c6818e161f64e036acaf7f2e3c5d47ce
Jean-Baptiste Pinalie <2850825+jybp@users.noreply.github.com>
2021-11-19 10:12:11 +0100

    insert pkg

--- main_test.go
+++ main_test.go
@@ -29,3 +29,26 @@ func TestExtract(t *testing.T) {
 	require.Equal(t, []string{"hello"}, names)
 	require.Equal(t, []int{12}, lines)
 }
+
+func TestPkg(t *testing.T) {
+	text := `package pkg
+
+	import (
+		"context"
+		"log"
+		"time"
+	
+		"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	)
+	
+	func Hello(ctx context.Context) {
+		span, ctx := tracer.StartSpanFromContext(ctx, "hello")
+		defer span.Finish()
+		time.Sleep(time.Second * 5)
+		log.Print("hello")
+	}
+`
+
+	res := pkg(text)
+	require.Equal(t, "pkg", res)
+}
